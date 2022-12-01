43d26a947d6c66e7ab411ed8cd8c7bd7f679a10a
NejiHyu <114939357+NejiHyu@users.noreply.github.com>
2022-12-01 12:05:21 -0500

    Create Main.java

---
+++ 07-string-puzzles/Main.java
@@ -0,0 +1,14 @@
+class Main {
+  public static void main(String[] args) {
+    System.out.println(conCat("abc","cat"));
+
+    
+  }
+
+   public static String conCat(Str first, Str second){
+    String str =" ";
+    string s1 = 
+    
+     
+  }
+}
