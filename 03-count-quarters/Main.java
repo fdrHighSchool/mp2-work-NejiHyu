ccf09b814ba8d9f2c3e49091b687d2fa85a25011
NejiHyu <114939357+NejiHyu@users.noreply.github.com>
2022-12-01 12:01:31 -0500

    Create Main.java

---
+++ 03-count-quarters/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+  public static void main(String args[]){  
+    public static int countQuarters(int cents) {
+      
+      string numQuarters = int numQuarters;
+      cents = cents % 100;
+      numQuarters = cents / 25;
+     
+      return numQuarters;
+  }  }
+}
